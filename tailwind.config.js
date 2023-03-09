d3dd554c84ea6dfed58ab8bfa488ca86ad9ce986
Abdullah-Naifeh <abdna59@gmail.com>
2023-03-09 17:22:10 +0300

    Add Hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/*.{html,js}"],
+  variants: {
+    textColor: ["responsive", "hover", "focus", "group-hover"],
+  },
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
